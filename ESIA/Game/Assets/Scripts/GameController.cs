6f1f5e3ac8ed45d8064931e0f555cc56914456ba
Pedro Correa <pedroorez@gmail.com>
2015-01-07 20:21:31 -0200

    Automatic Spawn Added

---
+++ ESIA/Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector2 spawnValues;
+	public float spawnWait = 1;
+	public float startWait = 1;
+	public int hazardCount = 10;
+
+	void Start(){
+		StartCoroutine(SpawWaves());
+	}
+
+	IEnumerator SpawWaves(){
+		for(int i=0; i< hazardCount; i++)
+		{
+			yield return new WaitForSeconds(startWait);
+
+			Vector2 spawnPosition = new Vector2(Random.Range(-spawnValues.x,spawnValues.x),spawnValues.y);
+			Quaternion spawnRotation = Quaternion.identity;
+
+			Instantiate (hazard, spawnPosition, spawnRotation);
+
+			yield return new WaitForSeconds(spawnWait);
+		}
+	}
+
+
+
+}
