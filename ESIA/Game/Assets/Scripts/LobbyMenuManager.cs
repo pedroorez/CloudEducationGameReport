6f994770c43a3e1a4324ec7ba0ea7e81bfcfa45e
Pedro Correa <pedroorez@gmail.com>
2015-02-08 21:20:57 -0200

    added loby scene and some buttons

---
+++ ESIA/Game/Assets/Scripts/LobbyMenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LobbyMenuManager : MonoBehaviour {
+
+	// scrollListCanvas
+	public GameObject lobbyMenuCanvas;
+	public GameObject gameList;
+	public LoaderManager loadManager;
+
+	// start game function
+	public void StartGame(){
+		Application.LoadLevel ("Lobby"); 
+	}
+
+	// show All Games List Canvas
+	public void showAllGameList(){
+		lobbyMenuCanvas.SetActive(false);
+		loadManager.MakeLogin();
+		loadManager.GenerateGameList();
+		gameList.SetActive(true);
+	}
+
+}
