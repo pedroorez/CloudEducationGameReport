470f3f44204774d0545c1d34b0daa6e161ee2ccf
Pedro Correa <pedroorez@gmail.com>
2015-04-19 13:58:43 -0300

    Add a session Factory function

---
+++ CloudGameReport/src/java/com/cloudgamereport/DAO/SFactory.java
@@ -0,0 +1,32 @@
+package com.cloudgamereport.DAO;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class SFactory {
+    
+        public static SessionFactory sessionFactory;
+    
+        // Session Factory Creator
+        public SFactory() throws Exception {
+        
+        if(sessionFactory == null){
+        
+            // A SessionFactory is set up once for an application
+            sessionFactory = new Configuration()
+                .configure() // configures settings from hibernate.cfg.xml
+                .buildSessionFactory();
+        }
+        
+        if(sessionFactory != null){
+        
+            // faz nada
+            
+        }
+
+        
+    }
+  
+    
+}
