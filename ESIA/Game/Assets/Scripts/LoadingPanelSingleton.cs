d776dcc93a70d2a21271c073f4e187b4a267dc06
Pedro Correa <pedroorez@gmail.com>
2015-06-28 01:57:17 -0300

    Added Loading Panel Singleton

---
+++ ESIA/Game/Assets/Scripts/LoadingPanelSingleton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingPanelSingleton : MonoBehaviour
+{
+    // Singleton
+    public static LoadingPanelSingleton singleton;
+
+    void Awake()
+    {
+        //If I am the first instance, make me the Singleton
+        if (singleton == null)
+        {
+            singleton = this;
+            DontDestroyOnLoad(this);
+        }
+        // If a Singleton already exists and you find another 
+        // reference in scene, destroy it!
+        else { if (this != singleton) Destroy(this.gameObject); }
+        gameObject.SetActive(false);
+    }
+}
